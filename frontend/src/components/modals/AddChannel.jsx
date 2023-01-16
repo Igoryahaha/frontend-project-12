2458902b2ee5cf50a3ac4d3d35e0fdd44766261d
Igor Kazantsev <rumata3411@gmail.com>
2023-01-16 16:34:49 +0300

    Убрал ошибку для проверки аналитики

--- frontend/src/components/modals/AddChannel.jsx
+++ frontend/src/components/modals/AddChannel.jsx
@@ -26,8 +26,6 @@ const AddChannel = () => {
 
   const hideHandle = () => {
     dispatch(UIActions.hideModal());
-    console.log(process.env, process.env.REACT_APP_ROLLBAR_TOKEN, process.env.NODE_ENV);
-    throw new Error('asdasd');
   };
 
   return (
