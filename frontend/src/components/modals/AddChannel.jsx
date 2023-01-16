e0b00040d2128011aeddccc8313c9df90e3f1663
Igor Kazantsev <rumata3411@gmail.com>
2023-01-16 16:18:43 +0300

    Проверка env

--- frontend/src/components/modals/AddChannel.jsx
+++ frontend/src/components/modals/AddChannel.jsx
@@ -26,6 +26,7 @@ const AddChannel = () => {
 
   const hideHandle = () => {
     dispatch(UIActions.hideModal());
+    console.log(process.env, process.env.REACT_APP_ROLLBAR_TOKEN, process.env.NODE_ENV);
     throw new Error('asdasd');
   };
 
